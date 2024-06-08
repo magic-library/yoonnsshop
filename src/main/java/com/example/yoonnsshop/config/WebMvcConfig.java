5eb8f8576c8d4633950fb7633a2944b87fed72be
yoon1 <o_ink@naver.com>
2024-06-08 16:22:20 +0900

    chore: remove comment

--- src/main/java/com/example/yoonnsshop/config/WebMvcConfig.java
+++ src/main/java/com/example/yoonnsshop/config/WebMvcConfig.java
@@ -1,9 +1,7 @@
 package com.example.yoonnsshop.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
